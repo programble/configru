b8bb9ee94cf6aa35f8ef1cfbcf8e3743b36dc25d
Curtis McEnroe <programble@gmail.com>
2011-06-22 12:23:32 -0400

    Added a ConfigHash#merge! function

--- lib/configru/confighash.rb
+++ lib/configru/confighash.rb
@@ -1,15 +1,25 @@
 module Configru
   class ConfigHash < Hash
     def initialize(hash={})
-      hash.each do |k, v|
-        v = ConfigHash.new(v) if v.is_a?(Hash)
-        self[k] = v
+      super
+      merge!(hash)
+    end
+    
+    def merge!(hash)
+      hash.each do |key, value|
+        if self.include?(key) && value.is_a?(Hash)
+          self[key].merge!(value)
+        elsif value.is_a?(Hash)
+          self[key] = ConfigHash.new(value)
+        else
+          self[key] = value
+        end
       end
     end
     
     def [](key)
       key = key.to_s if key.is_a?(Symbol)
-      super(key).is_a?(Hash) ? ConfigHash.new(super(key)) : super(key)
+      super(key)
     end
     
     def method_missing(key, *args)
