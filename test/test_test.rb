02427b4daadc230b0b95edd088a2cae70581f95f
Curtis McEnroe <programble@gmail.com>
2011-06-30 00:21:14 -0400

    Added Jeweler stuff

---
+++ test/test_test.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestTest < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
