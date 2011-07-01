311f5bcf9524fac6b95d6bd36e6aa6c20477fa23
Curtis McEnroe <programble@gmail.com>
2011-06-30 22:55:40 -0400

    Switched to RSpec (I think)

--- test/test_test.rb
+++
@@ -1,7 +0,0 @@
-require 'helper'
-
-class TestTest < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
-  end
-end
