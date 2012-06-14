db7b3e6f679e28dec66f0ca9fb3a9e11da8feb7a
Curtis McEnroe <programble@gmail.com>
2012-06-13 23:38:08 -0400

    Replace Option class with struct

--- lib/configru/option.rb
+++ lib/configru/option.rb
@@ -1,12 +1,3 @@
 module Configru
-  class Option
-    attr_accessor :type, :default, :validate, :transform
-
-    def initialize(type, default, validate, transform)
-      @type = type
-      @default = default
-      @validate = validate
-      @transform = transform
-    end
-  end
+  Option = Struct.new(:type, :default, :validate, :transform)
 end
