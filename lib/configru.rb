69d57b59b0725b993a1c9ea3c05420c138a24280
Curtis McEnroe <programble@gmail.com>
2012-06-17 18:48:14 -0400

    Only load non-empty files

--- lib/configru.rb
+++ lib/configru.rb
@@ -17,7 +17,7 @@ module Configru
   def self.reload
     loaded_files = []
     @files.each do |file|
-      if File.file?(file)
+      if File.file?(file) && File.size(file) > 0
         self.load_file(file)
         loaded_files << file
       end
