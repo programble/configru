9c1ab7aed3b12c0c476d462058a6f8eab00a25aa
Curtis McEnroe <programble@gmail.com>
2013-10-09 16:09:21 -0400

    Remove useless Configru.[]
    It can be handled by Configru.method_missing.

--- lib/configru.rb
+++ lib/configru.rb
@@ -5,10 +5,6 @@ module Configru
     @config = Config.new(*files, &block)
   end
 
-  def self.[](key)
-    @config[key]
-  end
-
   def self.method_missing(*args, &block)
     @config.send(*args, &block)
   end
