b628318198e0c2d56ab38829e3c2fa31e31ed9e5
Curtis McEnroe <programble@gmail.com>
2013-10-09 17:28:33 -0400

    Use Coveralls

--- Gemfile
+++ Gemfile
@@ -1,9 +1,9 @@
 source 'https://rubygems.org'
 
 group :development do
-  gem "rake", "~> 10.1.0"
-  gem "rspec", "~> 2.14.0"
-  gem "simplecov", "~> 0.7.0"
+  gem 'rake', '~> 10.1.0'
+  gem 'rspec', '~> 2.14.0'
+  gem 'coveralls', require: false
 end
 
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
 require 'bundler/setup'
 
 require 'simplecov'
+require 'coveralls'
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
 SimpleCov.start do
   add_filter '/spec/'
 end
