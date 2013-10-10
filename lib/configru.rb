b3c56db2ad67c570df8fdc6148e756da118a0a2d
Curtis McEnroe <programble@gmail.com>
2013-10-09 23:15:29 -0400

    Output and exit on error in Configru.load

--- lib/configru.rb
+++ lib/configru.rb
@@ -1,8 +1,22 @@
 require 'configru/config'
 
 module Configru
+  @raise_exceptions = false
+
+  def self.raise_exceptions
+    @raise_exceptions
+  end
+
+  def self.raise_exceptions=(x)
+    @raise_exceptions = x
+  end
+
   def self.load(*files, &block)
     @config = Config.new(*files, &block)
+  rescue OptionError => e
+    raise if @raise_exceptions
+    $stderr.puts "Configuration error #{e.message}"
+    exit 1
   end
 
   def self.method_missing(*args, &block)
--- spec/configru_spec.rb
+++ spec/configru_spec.rb
@@ -1,4 +1,14 @@
 describe Configru do
+  before do
+    @stderr = $stderr
+    @io = StringIO.new
+    $stderr = @io
+  end
+
+  after do
+    $stderr = @stderr
+  end
+
   it 'behaves like Config' do
     Configru.load do
       option :example, String, 'example'
@@ -7,5 +17,29 @@ describe Configru do
     Configru['example'].should == 'example'
     Configru.example.should == 'example'
     expect { Configru.idonotexist }.to raise_error(NoMethodError)
+  end
+
+  it 'allows access to raise_exceptions' do
+    Configru.raise_exceptions = false
+    Configru.raise_exceptions.should == false
+  end
+
+  it 'raises exceptions' do
+    Configru.raise_exceptions = true
+    expect do
+      Configru.load do
+        option_required :example, String
+      end
+    end.to raise_error(Configru::OptionRequiredError)
+  end
+
+  it 'exits with message' do
+    Configru.raise_exceptions = false
+    expect do
+      Configru.load do
+        option_required :example, String
+      end
+    end.to raise_error(SystemExit)
+    @io.string.should =~ /option required/
   end
 end
